fbfda4feb35ca6912b01d788e2950b3b198182f9
Entermain <105133513+Entermain@users.noreply.github.com>
2022-08-03 13:28:32 -0500

    Innitial commit
    Creación del código para el workshop

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://static.wikia.nocookie.net/memes-pedia/images/7/74/This_Is_Fine.png/revision/latest?cb=20170101154622&path-prefix=es"
+  );
+  await page.waitForTimeout(10000);
+  await page.screenshot({ path: "fine.png" });
+
+  await browser.close();
+})();
